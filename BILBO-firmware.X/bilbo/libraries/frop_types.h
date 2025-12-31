f8eec12a142d7f5fac322d7659e3ec9b10913c9b
AveryVi <main.averyvi@gmail.com>
2025-12-31 19:33:31 +0100

    frop fixes and addition of change profile

--- BILBO-firmware.X/bilbo/libraries/frop_types.h
+++ BILBO-firmware.X/bilbo/libraries/frop_types.h
@@ -13,7 +13,7 @@ struct short_error_message_s {
 
 // short error message (error code required)
 typedef union {
-    uint8_t data[6];
+    uint8_t data[5];
     uint8_t short_error_message_s;
 } short_error_message;
 
@@ -27,7 +27,7 @@ struct ok_response_s {
 
 // ok response (no intput required)
 typedef union {
-    uint8_t data[5];
+    uint8_t data[4];
     uint8_t ok_response_s;
 } ok_response ;
 
@@ -42,15 +42,17 @@ struct tuning_data_s {
     uint8_t end_of_header;
     uint8_t block_length_setting;
     uint8_t block_data_setting;
-    uint8_t block_length_note;
-    uint16_t block_data_note;
+    uint8_t block_length_note_oct;
+    uint8_t block_data_note_oct;
+    uint8_t block_length_note_pos;
+    uint8_t block_data_note_pos;
     uint8_t block_length_freq;
     uint16_t block_data_freq;
     uint8_t block_length_diff;
     uint8_t block_data_diff;
 };
 
-// tuning data (required: NoF = 4, "T" [1], note [2], frequency [2], tune offset [1])
+// tuning data (required: NoF = 5, "T" [1], note octive [1], note position in octive [1], frequency [2], tune offset [1])
 typedef union {
     uint8_t data[16];
     uint8_t tuning_data_s;
@@ -71,30 +73,32 @@ struct range_change_s {
 
 // range change (required: NoF = 2, "R" [1], range [1])
 typedef union {
-    uint8_t data[10];
+    uint8_t data[9];
     uint8_t tuning_data_s;
 } range_change ;
 
 
-/*
 struct change_profile_s {
     uint8_t start_of_message;
     uint8_t frop_message_type;
     uint8_t frop_message_format;
     uint8_t number_of_fields;
+    uint16_t length_of_data;
+    uint16_t header_checksum;
     uint8_t end_of_header;
     uint8_t block_length_setting;
     uint8_t block_data_setting;
-    uint8_t block_length_range;
-    uint8_t block_data_range;
+    uint8_t block_length_ref;
+    uint16_t block_data_ref;
+    uint8_t block_length_ref_oct;
+    uint8_t block_data_ref_oct;
 };
 
-// range profile (required: NoF = , )
+// change profile (required: NoF = 2, "P" [1], reference note [2], reference note octive [1] )
 typedef union {
-    uint8_t data[10];
+    uint8_t data[16];
     uint8_t change_profile_s;
 } change_profile ;
-*/
 
 #define FROP_MSG_NULL 0
 #define FROP_MSG_D_NULL 0x10
--- BILBO-firmware.X/bilbo/low_level/frop.c
+++ BILBO-firmware.X/bilbo/low_level/frop.c
@@ -17,22 +17,23 @@ short_error_message build_short_error_message(uint8_t error_code) {
 }
 
 ok_response build_ok_response() {
-    short_error_message message;
+    ok_response message;
     message.data = {'F', 'R', 0x1, 'D'};
     return message;
 }
 
-tuning_data build_tuning_data(uint8_t note[2], uint16_t frequency, uint8_t tune_offset) {
+tuning_data build_tuning_data(uint8_t note_octive, uint8_t note_position_in_octive, uint16_t frequency, uint8_t tune_offset) {
     uint8_t frequency_byte_one = (uint8_t)(frequency >> 8);
     uint8_t frequency_byte_two = (uint8_t)frequency;
-    short_error_message message;
-    message.data = {'F', 'D', 0x10, 4, 'D', 1, "T", 2, note[0], note[1], 2, frequency_byte_one, frequency_byte_two, 1, tune_offset};
+    
+    tuning_data message;
+    message.data = {'F', 'D', 0x10, 5, 'D', 1, "T", 1, note_octive, 1, note_position_in_octive, 2, frequency_byte_one, frequency_byte_two, 1, tune_offset};
     return message;
 }
 
 range_change build_range_change(uint8_t range) {
-    short_error_message message;
-    message.data = {'F', 'D', 0x10, 2, 'D', 1, "C", 1, range};
+    range_change message;
+    message.data = {'F', 'D', 0x10, 2, 'D', 1, "R", 1, range};
     return message;
 }
 
