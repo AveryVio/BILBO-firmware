3ad6c58166e10335c977fd0f98c7b53e6dc396c0
AveryVi <main.averyvi@gmail.com>
2025-12-31 21:28:32 +0100

    continued work on parsing frop messages

--- BILBO-firmware.X/bilbo/low_level/frop.c
+++ BILBO-firmware.X/bilbo/low_level/frop.c
@@ -48,8 +48,13 @@ uint8_t decide_incoming_message_type(uint8_t *message){
     } else return FROP_MSG_NULL;
 }
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+uint8_t last_sent_message = FROP_MSG_NULL;
+
 void parse_frop_message(){
     uint8_t message[128];
+    
+    if(message[0] != 'F');//bad message
     
     switch( decide_incoming_message_type(&message) ){
         case FROP_MSG_NULL:
@@ -65,6 +70,28 @@ void parse_frop_message(){
             // handle error
             break;
         case FROP_MSG_D_PROFILE_CHANGE:
+            change_profile frop_organised_message;
+            for(uint8_t i = 16; i >= 0; i--) frop_organised_message[i] = message[i];
+            
+            // this is not proper sanitation, but i don't have enough time to care to make an effecient way for it. let's just collectively pretend that bluetooth is perfect and has no issues at all
+            
+            if(frop_organised_message.structured.number_of_fields != 3); // throw error
+            
+            if(frop_organised_message.structured.length_of_data != 4); // throw error
+            
+            if(frop_organised_message.structured.header_checksum != 0xB1); // throw error
+
+            if(frop_organised_message.structured.block_length_setting != 1); // throw error
+
+            
+            if(frop_organised_message.structured.block_length_ref != 2); // throw error
+            
+            if(frop_organised_message.structured.block_length_ref_oct != 1); // throw error
+
+            //check ref oct validity
+            //check ref validity
+            //check 
+            
             break;
         case FROP_MSG_R_OK:
             // handle ok of last message
